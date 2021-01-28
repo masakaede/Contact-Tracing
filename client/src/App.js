34324163c2853e7edcdd7abbe34914bb3b9f4c10
masakaede <69030790+masakaede@users.noreply.github.com>
2021-01-28 22:26:10 +1100

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import CheckIn from "./components/pages/CheckIn";
 import CheckOut from "./components/pages/CheckOut";
 import Management from "./components/pages/Management";
-import Navbar from "./components/Navbar"
 import NavTabs from "./components/Navtabs";
 import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper"
 import './App.css';
 
 function App() {
@@ -14,17 +12,14 @@ function App() {
     <Router>
       <div>
         <NavTabs />
-        <Wrapper>
-          <Route exact path="/" component={CheckIn} />
-          <Route exact path="/checkIn" component={CheckIn} />
-          <Route exact path="/checkOut" component={CheckOut} />
-          <Route exact path="/management" component={Management} />
-        </Wrapper>
+        <Route exact path="/" component={CheckIn} />
+        <Route exact path="/checkIn" component={CheckIn} />
+        <Route exact path="/checkOut" component={CheckOut} />
+        <Route exact path="/management" component={Management} />
         <Footer />
       </div>
     </Router>
   );
 }
-
 
 export default App;
